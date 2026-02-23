d76524385819f037fa173a5ba558a71209c00d55
Hatim-Fangour <hatimfangour87@gmail.com>
2026-02-23 16:18:15 +0000

    with backend

--- fixilya-backend/fixilya-microservices/services/user-service/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticate } = require('../middleware/auth');
-const userController = require('../controllers/userController');
-
-// Protected routes
-router.delete('/account', authenticate, userController.deleteAccount);
-
-module.exports = router;
