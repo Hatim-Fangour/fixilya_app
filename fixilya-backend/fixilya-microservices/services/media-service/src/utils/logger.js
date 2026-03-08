6dbd8c876c61f387e9ba5eae3e6df4814f6e35f8
Hatim-Fangour <hatimfangour87@gmail.com>
2026-03-08 22:04:18 +0000

    chore: move backend to separate repo

--- fixilya-backend/fixilya-microservices/services/media-service/src/utils/logger.js
+++
@@ -1,3 +0,0 @@
-// Re-export the shared Winston logger.
-const path = require('path');
-module.exports = require(path.join(__dirname, '../../../../shared/utils/logger'));
--- fixilya-backend/fixilya-microservices/services/notification-service/src/config/firebase.js
+++
@@ -1,5 +0,0 @@
-// Re-export shared Firebase Admin SDK configuration.
-// All services use the same Firebase project; centralising init in shared/
-// prevents duplicate app registration across requires.
-const path = require('path');
-module.exports = require(path.join(__dirname, '../../../../shared/config/firebase'));
